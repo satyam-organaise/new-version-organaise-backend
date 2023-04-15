398b7b069a2036e08a1e709c69251d958e69a54a
satyam <satyamsonker1010@gmail.com>
2023-04-15 18:42:25 +0530

    socker implemet iin backedn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import messageRoute from "./routes/messageRoute.js";
 
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import { Server } from 'socket.io';
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -43,15 +44,66 @@ app.use(cors());
 app.use('/api', AuthRouter);//////login, signup , forget password, change password, resend otp
 app.use("/api/v2/file", FilesRoute);
 app.use("/api/v2/company", CompanyRoute);
-app.use("/api/v2/folder",FolderRoute);
+app.use("/api/v2/folder", FolderRoute);
 app.use("/api/v2/chat", chatRoute);
 app.use("/api/v2/message", messageRoute);
 
 
-
 const expressServer = app.listen(process.env.APP_PORT, () => {
   connect();
-  console.log(`Running on ${process.env.APP_PORT}`);
+  console.log("Connected to backend");
 });
 
+
+
+
+const io = new Server(expressServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: [
+       process.env.PRDO_URL
+      //"http://localhost:3000"
+    ],
+    path: "/" + "socketio"
+    //process.env.NODE_ENV === 'production' ? process.env.PRDO_URL : process.env.DEV_URL
+  }
+})
+
+
+// const expressServer = app.listen(process.env.APP_PORT, () => {
+//   connect();
+//   console.log(`Running on ${process.env.APP_PORT}`);
+// });
+
+
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  ///// Here setup the login user
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  })
+
+  /////Check which chat is active;
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room");
+  })
+
+  ////// When user typeing start
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  ///// Check new recived message
+  socket.on("new message", (newMessageRecived) => {
+    var chat = newMessageRecived.chat;
+    if (!chat.users) return console.log("chat.user not defined");
+    chat.users.forEach(user => {
+      if (user._id === newMessageRecived.sender._id) return;
+      socket.in(user._id).emit("message recived", newMessageRecived);
+    });
+  })
+
+})
 
